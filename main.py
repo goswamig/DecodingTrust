1aaeeb6d3f3750fb4ce5ddc2278d4b2ae1f01cdc
Gautam Kumar <gauta@amazon.com>
2023-06-24 15:45:53 -0700

    local deployment working

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import subprocess
+import sys
 
 app = FastAPI()
 
@@ -13,10 +14,20 @@ class GPTRequest(BaseModel):
 @app.post("/generate/")
 def generate_text(request: GPTRequest):
     command = f"python adv-glue-plus-plus/gpt_eval.py --model {request.model} --key {request.key} --data-file {request.data_file} --out-file {request.out_file}"
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
-    if result.returncode == 0:
+
+    # stdout and stderr goes to stdout 
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+
+    # Stream the subprocess output
+    for line in iter(process.stdout.readline, ''):
+        sys.stdout.write(line)
+        sys.stdout.flush()
+
+    process.communicate()  # Wait for the process to finish
+    if process.returncode == 0:
         return {"success": True, "output": result.stdout}
     else:
         return {"success": False, "error": result.stderr}
+
 
 
