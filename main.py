2deacbe8a531ed648dcc26fdd96a4b4df3c00bfa
Gautam Kumar <gauta@amazon.com>
2023-06-25 14:52:03 -0700

    Adding dummy version of DB

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ class GPTRequest(BaseModel):
     data_file: str
     out_file: str
 
+
+jobs = []
+
 @app.post("/generate/")
 def generate_text(request: GPTRequest):
     command = f"python adv-glue-plus-plus/gpt_eval.py --model {request.model} --key {request.key} --data-file {request.data_file} --out-file {request.out_file}"
@@ -29,5 +32,22 @@ def generate_text(request: GPTRequest):
     else:
         return {"success": False, "error": result.stderr}
 
+# TODO: Add logic to connect to DB
+@app.get("/job/describe/{job_id}")
+def describe_job(job_id: str):
+    # Implement the logic to describe a job based on the job_id
+    return {"job_id": job_id, "description": "Job description goes here"}
 
+@app.post("/job/start/{job_id}")
+def start_job(job_id: str):
+    # Implement the logic to start a job based on the job_id
+    return {"job_id": job_id, "status": "Job started"}
 
+@app.post("/job/stop/{job_id}")
+def stop_job(job_id: str):
+    # Implement the logic to stop a job based on the job_id
+    return {"job_id": job_id, "status": "Job stopped"}
+
+@app.get("/jobs/")
+def list_jobs():
+    return {"jobs": jobs}
